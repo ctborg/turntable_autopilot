8e79d18738c515f5d3fa83e83514aa90ec24ab04
afri <alex@onilabs.com>
2011-06-21 14:28:50 +0200

    pin apollo to version 0.12

--- src/loader.js
+++ src/loader.js
@@ -9,6 +9,5 @@ chrome.extension.sendRequest({'action':'fetchResource', url:url}, function(txt) 
   // inject apollo runtime
   elem = document.createElement("script");
   document.getElementsByTagName("head")[0].appendChild(elem);
-  elem.src = "http://code.onilabs.com/apollo/latest/oni-apollo.js";
+  elem.src = "http://code.onilabs.com/apollo/0.12/oni-apollo.js";
 });
-
